24269b154d3a4babcce2f4911cdcb6486cbbb1b7
eric <p-chao@hotmail.com>
2024-10-05 20:52:20 +0800

    init with rtsp demo

---
+++ README.mk
@@ -0,0 +1,24 @@
+RKVsionDemo Project
+===
+demo project for computer vision and image processing
+
+### Functions
+
+| Category | Name | Dtype | Model Link | Support platform | Speed |
+| --- | --- | --- | --- | --- | --- |
+
+### Features
+
+* Output with rtsp
+
+### Usage
+
+before lunch the project, the following envirionment should be prepared:
+
+* configure rknpu driver and rknpu runtime
+* install gcc, cmake, openssh, opencv
+
+### Thanks
+
+* rtsp_server project: [https://github.com/xiaodongzhang1025/RtspServer]
+* rknn model zoo:  [https://github.com/airockchip/rknn_model_zoo]
